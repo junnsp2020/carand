95c9cf6e93cd92fa17077afed11d26341edf7a63
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-03-09 03:07:44 +0900

    [add] カテゴリー画面(index)

--- app/controllers/administers/categories_controller.rb
+++ app/controllers/administers/categories_controller.rb
@@ -1,8 +1,19 @@
 class Administers::CategoriesController < ApplicationController
+  def new
+  end
+
   def index
+  	@category = Category.new
+  	@categories = Category.all
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to administers_categories_path
+    else
+      render :index
+    end
   end
 
   def edit
@@ -10,4 +21,8 @@ class Administers::CategoriesController < ApplicationController
 
   def update
   end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :status)
 end
