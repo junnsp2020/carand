0fba78d1c7e4754876d5b747393ebf95c8357b78
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-03-08 19:46:21 +0900

    [add] 商品機能(edit)

--- app/controllers/users/products_controller.rb
+++ app/controllers/users/products_controller.rb
@@ -22,9 +22,16 @@ class Users::ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
   end
 
   def destroy
