10af90adee19550873b1cee5153add91c04661c1
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-03-11 06:24:45 +0900

    [fix] ルーティング変更(barter_requests)

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ scope module: :users do
 		resource :favorites
 		resource :reviews
 		resources :tradings
-		resources :barter_requests, except: [:new, :edit, :update]
+		resources :barter_requests, except: [:edit, :update]
 	end
 	resources :tradings, except: [:destroy] do
 		resource :trading_messages
