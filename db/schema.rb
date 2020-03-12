8c1658bbf842a6c65cf462edf76ea9454c34b00e
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-03-12 21:19:37 +0900

    [add]　交換リクエスト(buyer_idとseller_idカラムの追加/特定のユーザーの交換リクエストを承認するため)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_034054) do
+ActiveRecord::Schema.define(version: 2020_03_12_121058) do
 
   create_table "administers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2020_03_12_034054) do
     t.integer "product_condition", default: 0
     t.text "comment"
     t.boolean "propriety"
+    t.integer "seller_id"
+    t.integer "buyer_id"
   end
 
   create_table "blog_comments", force: :cascade do |t|
