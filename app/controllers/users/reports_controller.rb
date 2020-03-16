239a65ef5f48ab5b59f071c14c715ce973e5a306
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-03-16 10:18:44 +0900

    [Add] 通報(コントローラー/params設定&通報が保存されるようcreateの部分に追記)

--- app/controllers/users/reports_controller.rb
+++ app/controllers/users/reports_controller.rb
@@ -5,12 +5,18 @@ class Users::ReportsController < ApplicationController
   end
 
   def create
+  	@product = Product.find(params[:product_id])
   	@report = Report.new(report_params)
     @report.user_id = current_user.id
     if @report.save
       redirect_to product_path(@product.id)
     else
-      render :index
+      render :new
     end
+  end
+
+  private
+  def report_params
+    params.require(:report).permit(:product_id, :user_id, :subject, :content)
   end
 end
