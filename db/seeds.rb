fb40c9f95c8008046d209e745d7fdd9ea8b9b097
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-04-07 01:20:37 +0900

    [Fix] 商品(newページ/意図しないフラッシュメッセージが表示されていた為、修正)

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ Category.create([
   	{name: "家具"}
 ])
 
-Product.create([
+Product.create!([
 	{user_id: 1 , category_id: 1, name: "商品 1", introduction: "大切に使用してきた商品です。梱包は厳重に致しますのでご安心ください。", price: 5000},
 	{user_id: 2 , category_id: 2, name: "商品 2", introduction: "大切に使用してきた商品です。梱包は厳重に致しますのでご安心ください。", price: 6500, propriety: 1},
 	{user_id: 3 , category_id: 3, name: "商品 3", introduction: "大切に使用してきた商品です。梱包は厳重に致しますのでご安心ください。", price: 8000, sale_status: 1},
