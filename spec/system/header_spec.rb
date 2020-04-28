5e98ed2dff88f2351baabb7e28a54998540ac5a5
junnsp2020 <junnsp2011@yahoo.co.jp>
2020-04-28 19:06:02 +0900

    [Remove] 不要ファイルを削除

--- spec/system/header_spec.rb
+++ spec/system/header_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "Header", type: :system do
-  describe "ヘッダーのテスト" do
-    describe "ログインしていない場合" do
+    describe "ヘッダーのテスト" do 
       before do
+        @user = User.create!(last_name: "カランド", first_name: "1号", last_name_kana: "カランド", first_name_kana: "イチゴウ", postcode: 1111111, prefecture_code: "カランド県", address_city: "カランド市", address_street: "カランド町", email: "c@c" , nickname: "カランド１号", password: 111111, phone_number: "08011111111")
         visit root_path
       end
       context "ヘッダーの表示を確認" do
@@ -18,6 +18,36 @@ RSpec.describe "Header", type: :system do
           expect(page).to have_content "新規会員登録"
         end
       end
+      context "ログイン後" do
+        subject { page }
+          it "「買った！」リンクが表示される" do
+              visit new_user_session_path
+              fill_in 'user[email]', with: @user.email
+              fill_in 'user[password]', with: @user.password
+              click_button 'ログイン'
+              expect(page).to have_content "買った！"
+          end
+          it "「売れた！」リンクが表示される" do
+              visit new_user_session_path
+              fill_in 'user[email]', with: @user.email
+              fill_in 'user[password]', with: @user.password
+              click_button 'ログイン'
+              expect(page).to have_content "売れた！"
+          end
+          it "「交換！」リンクが表示される" do
+              visit new_user_session_path
+              fill_in 'user[email]', with: @user.email
+              fill_in 'user[password]', with: @user.password
+              click_button 'ログイン'
+              expect(page).to have_content "交換！"
+          end
+          it "「マイページ」リンクが表示される" do
+              visit new_user_session_path
+              fill_in 'user[email]', with: @user.email
+              fill_in 'user[password]', with: @user.password
+              click_button 'ログイン'
+              expect(page).to have_content "マイページ"
+          end
+      end
     end
-  end
 end
